09de342d05af28e90b84f1e9a31f7270581e2269
edygg <efgm1024@gmail.com>
2022-12-27 04:38:50 +0000

    Deploying to gh-pages from @ edygg/personal-page@92e8b52888d4dff7f2bbcf3d68ccfeca553501bb 🚀

---
+++ _next/static/StmhWTbCp5ib-4v4UqT4i/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/61-d3d383d48b3e83fd.js","static/chunks/pages/index-cb9fdc92459a576a.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
